629365e4795978e6fa27412d69d15dc48ee51e84
the-electro-punk <88555154+the-electro-punk@users.noreply.github.com>
2021-11-30 20:22:42 -0600

    practiced handlebars

---
+++ handelbars_practice.js
@@ -0,0 +1,16 @@
+function fill_template() {
+    var data = {
+        title: 'why hadlebars are cool',
+        list: [
+            {name: "you can generate stuff"},
+            {name: "Detroit Smash!!!"},
+            {name: "Cyborg Zelda"},
+            {name: "handlebars"},
+            {name: "Luigi x Daisy 4eva!"}
+        ],
+        footer: "this is a footer"
+    };
+    var template = Handlebars.compile(document.querySelector("#template"));
+    var filled = template(data);
+    document.querySelector("#output").innerHTML = filled;
+}
