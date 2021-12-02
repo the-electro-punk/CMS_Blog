0e1f4850c9e69945bec8a88838759edc1a35cd2e
the-electro-punk <88555154+the-electro-punk@users.noreply.github.com>
2021-12-01 18:35:22 -0600

    updated index files and added blog page

---
+++ createBlog.js
@@ -0,0 +1,2 @@
+let title = document.getElementById('inputTitle')
+let story = document.getElementById('inputText')
---
+++ index.js
@@ -0,0 +1,7 @@
+const addBTN = document.getElementById('newBlog')
+
+addBTN.addEventListener('click', newBlog)
+
+function newBlog() {
+    window.location='C:/Users/Ben/Desktop/_Coding BootCamp/_weeks lessons/week 14/CMS_Blog/blogCreate.html'
+}
