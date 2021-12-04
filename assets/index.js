082f94b30cd48ed5a3c428360ccd175318382222
the-electro-punk <88555154+the-electro-punk@users.noreply.github.com>
2021-12-03 20:02:41 -0600

    updated code and practicing handlebars

--- assets/index.js
+++ assets/index.js
@@ -3,5 +3,5 @@ const addBTN = document.getElementById('newBlog')
 addBTN.addEventListener('click', newBlog)
 
 function newBlog() {
-    window.location='C:/Users/Ben/Desktop/_Coding BootCamp/_weeks lessons/week 14/CMS_Blog/newBlog.handlebars'
+    window.location='C:/Users/Ben/Desktop/_Coding BootCamp/_weeks lessons/week 14/CMS_Blog/blogCreate.html'
 }
